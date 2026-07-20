6cd5637fe211cfa5d72a6faa2e7d248e6471c540
Fred-tech100 <emmanuelfredm@gmail.com>
2026-07-20 15:03:05 +0300

    Optimize Tailwind content pattern to exclude node_modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   content: [
     "./**/*.html",
-    "./**/*.js",
+    // Only scan JS files in your project, not node_modules
+    "./js/**/*.js",
+    // If you have other JS files outside the js folder, add them here
+    // "./*.js",
     "./**/*.css"
   ],
   theme: {
